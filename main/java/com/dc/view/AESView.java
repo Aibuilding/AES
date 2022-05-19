a416b0987a8b912386b1cfe0c0070459463ea505
艾强 <2727269416@qq.com>
2022-05-19 20:19:23 +0800

    完成

--- main/java/com/dc/view/AESView.java
+++ main/java/com/dc/view/AESView.java
@@ -40,16 +40,18 @@ public class AESView extends JDialog {
     private JTextField textField5;
     private JTextArea textArea5;
     private JLabel 控制台IP;
+    private JButton 发送文件Button;
     private JButton 接收文件Button;
-    private JTextField textField6;
-    private JButton 发送文件Button1;
+    private JTextField 发送TxtTextField;
+    private JTextField 接收TxtTextField;
+
     private boolean receiveState = false;
 
     private static String ip;
     private static String message;
 
     private static RecieveThread rt;
-    ;
+    private static RecieveFileThread rft;
 
     public AESView() {
         setContentPane(contentPane);
@@ -195,12 +197,87 @@ public class AESView extends JDialog {
                 }
             }
         });
+        发送文件Button.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                try {
+                    //                位数
+                    int selectedItem = Integer.parseInt((String) comboBox3.getSelectedItem());
+                    //                填充
+                    Object selectedItem1 = comboBox2.getSelectedItem();
+                    //                模式
+                    Object selectedItem2 = comboBox1.getSelectedItem();
+                    //                合成
+                    String type = "AES/" + selectedItem2 + "/" + selectedItem1;
+                    //                    偏移量
+                    byte[] bytes = "1234567812345678".getBytes("UTF-8");
+                    //              密钥
+                    String text = textField5.getText();
+                    //              明文
+                    FileInputStream fileInputStream = new FileInputStream(new File(发送TxtTextField.getText()));
+                    byte[] bytes1 = new byte[1024];
+                    int read = 0;
+                    String context = "";
+                    while ((read = fileInputStream.read(bytes1)) > 0) {
+                        String s = new String(bytes1, 0, read);
+                        context = context + s;
+                    }
+                    if (selectedItem2.equals("ECB") || selectedItem2.equals("CFB")) {
+                        bytes = null;
+                    }
+                    long startTime = System.nanoTime();
+                    bytes1 = AESUtil.encryptOrdecrypt(true, context.getBytes("UTF-8"), text, bytes, selectedItem, type);
+                    long endTime = System.nanoTime(); //获取结束时间
+                    String s = Base64.getEncoder().encodeToString(bytes1);
+                    //向局域网发送密文
+                    if (!对方IP.getText().equals("")) {
+                        ip = 对方IP.getText();
+                    }
+                    message = s;
+                    SendThread st = new SendThread();
+                    st.start();
+                    textArea5.setText(String.valueOf((endTime - startTime)));
+                } catch (UnsupportedEncodingException unsupportedEncodingException) {
+                    unsupportedEncodingException.printStackTrace();
+                } catch (InvalidAlgorithmParameterException invalidAlgorithmParameterException) {
+                    invalidAlgorithmParameterException.printStackTrace();
+                } catch (NoSuchPaddingException noSuchPaddingException) {
+                    noSuchPaddingException.printStackTrace();
+                } catch (IllegalBlockSizeException illegalBlockSizeException) {
+                    JOptionPane.showMessageDialog(null, "此模式不能选择NOthing填充", "填充错误", JOptionPane.ERROR_MESSAGE);
+                } catch (NoSuchAlgorithmException noSuchAlgorithmException) {
+                    noSuchAlgorithmException.printStackTrace();
+                } catch (BadPaddingException badPaddingException) {
+                    badPaddingException.printStackTrace();
+                } catch (InvalidKeyException invalidKeyException) {
+                    invalidKeyException.printStackTrace();
+                } catch (FileNotFoundException ex) {
+                    throw new RuntimeException(ex);
+                } catch (IOException ex) {
+                    throw new RuntimeException(ex);
+                }
+            }
+        });
+        接收文件Button.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                接收文件Button.setText("正在监听");
+                发送文件Button.setEnabled(false);
+                发送文件Button.setEnabled(false);
+                接收Button.setEnabled(false);
+                发送Button.setEnabled(false);
+                rft = new RecieveFileThread();
+                rft.start();
+            }
+        });
         接收Button.addActionListener(new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent e) {
                 接收Button.setText("正在监听");
                 接收Button.setEnabled(false);
                 发送Button.setEnabled(false);
+                接收文件Button.setEnabled(false);
+                发送文件Button.setEnabled(false);
                 rt = new RecieveThread();
                 rt.start();
             }
@@ -494,6 +571,71 @@ public class AESView extends JDialog {
                 s.close();
                 ss.close();
                 接收Button.setText("从网络解密接收");
+                接收Button.setEnabled(true);
+                发送Button.setEnabled(true);
+                接收文件Button.setEnabled(true);
+                发送文件Button.setEnabled(true);
+            } catch (IOException e) {
+                传输数据textArea.setText("监听进程出错");
+                throw new RuntimeException(e);
+            }
+        }
+    }
+
+    class RecieveFileThread extends Thread {
+        public void run() {
+            try {
+                ServerSocket ss = new ServerSocket(10010);
+                传输数据textArea.setText("文件监听端口等待数据......");
+                Socket s = ss.accept();
+                BufferedReader br = new BufferedReader(new InputStreamReader(s.getInputStream()));
+                //读取客户端发送来的消息
+                String mess = br.readLine();
+                System.out.println(mess);
+                try {
+                    //                位数
+                    int selectedItem = Integer.parseInt((String) comboBox3.getSelectedItem());
+                    //                填充
+                    Object selectedItem1 = comboBox2.getSelectedItem();
+                    //                模式
+                    Object selectedItem2 = comboBox1.getSelectedItem();
+                    //                合成
+                    String type = "AES/" + selectedItem2 + "/" + selectedItem1;
+                    //                    偏移量
+                    byte[] bytes = "1234567812345678".getBytes("UTF-8");
+                    //              密钥
+                    String text = textField5.getText();
+                    if (selectedItem2.equals("ECB") || selectedItem2.equals("CFB")) {
+                        bytes = null;
+                    }
+                    byte[] text1 = Base64.getDecoder().decode(mess);
+                    byte[] bytes1 = AESUtil.encryptOrdecrypt(false, text1, text, bytes, selectedItem, type);
+                    String str = new String(bytes1);
+                    FileOutputStream fileOutputStream = new FileOutputStream(new File(接收TxtTextField.getText()));
+                    fileOutputStream.write(bytes1);
+                    传输数据textArea.setText("接收到加密文件，存储到"+接收TxtTextField.getText());
+
+                } catch (UnsupportedEncodingException unsupportedEncodingException) {
+                    unsupportedEncodingException.printStackTrace();
+                } catch (InvalidAlgorithmParameterException invalidAlgorithmParameterException) {
+                    invalidAlgorithmParameterException.printStackTrace();
+                } catch (NoSuchPaddingException noSuchPaddingException) {
+                    noSuchPaddingException.printStackTrace();
+                } catch (IllegalBlockSizeException illegalBlockSizeException) {
+                    illegalBlockSizeException.printStackTrace();
+                } catch (NoSuchAlgorithmException noSuchAlgorithmException) {
+                    noSuchAlgorithmException.printStackTrace();
+                } catch (BadPaddingException badPaddingException) {
+                    传输数据textArea.setText("密钥可能错误");
+                } catch (InvalidKeyException invalidKeyException) {
+                    invalidKeyException.printStackTrace();
+                }
+                br.close();
+                s.close();
+                ss.close();
+                接收文件Button.setText("接收文件");
+                接收文件Button.setEnabled(true);
+                发送文件Button.setEnabled(true);
                 接收Button.setEnabled(true);
                 发送Button.setEnabled(true);
             } catch (IOException e) {
